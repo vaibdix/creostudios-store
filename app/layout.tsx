ee4a733073fd6945f750c80d3b262f60d9d46bac
vaibdix <dixitvaib@gmail.com>
2024-05-28 10:23:33 +0530

    feat : added loading top bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import NextTopLoader from "nextjs-toploader";
 
 import ModalProvider from "@/providers/modal-provider";
 import ToastProvider from "@/providers/toast-provider";
@@ -36,6 +37,21 @@ export default function RootLayout({
           <ToastProvider />
           <ModalProvider />
           <Navbar />
+          <NextTopLoader
+            color="#f46e6f"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={true}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #f46e6f,0 0 5px #f46e6f"
+            template='<div class="bar" role="bar"><div class="peg"></div></div>
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
           {children}
           <CtaSection />
           <Footer />
